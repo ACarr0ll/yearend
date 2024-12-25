5f30dd59539c75e4135cb02ad64c35ef09978226
ACarr0ll <42918684+ACarr0ll@users.noreply.github.com>
2024-12-25 11:49:45 -0600

    fixed database connection

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const { createTask } = require('./models/task.model.js');
 const cors = require('cors');
+const HttpsProxyAgent = require('https-proxy-agent');
 
 const app = express();
 app.use(cors());
 app.use(express.json())
+
+// Configure Proximo
+if (process.env.PROXIMO_URL) {
+    const proxyAgent = new HttpsProxyAgent(process.env.PROXIMO_URL);
+    app.set('trust proxy', true);
+    app.use((req, res, next) => {
+        req.agent = proxyAgent;
+        next();
+    });
+}
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server running`);
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World! Testing');
